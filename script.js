508c2d60cc2189304c6b19045c17bbfd542124ea
1nottoday <xxxfenix22@gmail.com>
2026-07-06 02:18:11 +0800

    2

---
+++ script.js
@@ -0,0 +1,6 @@
+const grid = document.getElementById("brandsGrid");
+const button = document.getElementById("hiddencontent")
+
+button. addEventListener("click", () => {
+    grid.classList.toggle("open");
+});
