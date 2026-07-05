ee4fb269015b9014efe34ca257ae4707bea97297
1nottoday <xxxfenix22@gmail.com>
2026-07-06 03:37:29 +0800

    4

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const button = document.getElementById("hiddencontent")
 button. addEventListener("click", () => {
     grid.classList.toggle("open");
 });
+
+const swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+});
