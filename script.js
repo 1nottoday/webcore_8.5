ae4b58066cdc548870647d3c20897e1c7edfb687
1nottoday <xxxfenix22@gmail.com>
2026-07-07 00:03:12 +0800

    2

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const grid = document.getElementById("brandsGrid");
 const button = document.getElementById("hiddencontent")
+const img = button.querySelector("img"); 
 
 button. addEventListener("click", () => {
     grid.classList.toggle("open");
+
+  if (grid.classList.contains("open")) {
+    img.src = "./images/up.svg";
+  } else {
+    img.src = "./images/down.svg";
+  }
 });
 
 const swiper = new Swiper('.swiper', {
